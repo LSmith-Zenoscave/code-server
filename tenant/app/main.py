6ba752d138441c04a46a4764fae0cab06ee55fe8
LSmith-Zenoscave <lsmith@zenoscave.com>
2020-01-21 16:48:36 +0000

    lowercase docker issue
    Signed-off-by: LSmith-Zenoscave <lsmith@zenoscave.com>

--- tenant/app/main.py
+++ tenant/app/main.py
@@ -1,15 +1,11 @@
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse, RedirectResponse
-from starlette.routing import Route
-from starlette.templating import Jinja2Templates
-from starlette.background import BackgroundTask
-
-from io import BytesIO
-
-import os
-import docker
 import asyncio
-import logging
+import os
+
+import docker
+from starlette.applications import Starlette
+from starlette.background import BackgroundTask
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
 
 host = os.environ["IDE_SUBDOMAIN"] + "." + os.environ["DOMAIN_NAME"]
 docker_network = f"{os.environ['COMPOSE_PROJECT_NAME']}_default"
@@ -20,9 +16,10 @@ client = docker.from_env()
 
 states = {}
 
+
 @app.route('/', methods=["GET"])
 async def ide(request):
-    user_id = request.headers['remote-user']
+    user_id = request.headers['remote-user'].lower()
     states[user_id] = states.get(user_id, 'STARTING')
     task = BackgroundTask(start_code_server, user_id)
     message = "Please wait... your browser will auto refresh when ready"
@@ -39,48 +36,48 @@ async def ide(request):
         resp.set_cookie('remote-user', user_id, 10000, 10000)
         return resp
 
-async def start_code_server(user_id):
-    code_name = f'code-{user_id}'
+
+async def start_code_server(id):
+    code_name = f'code-{id}'
     conf = f"""
 [http]
   [http.routers]
-    [http.routers.{user_id}]
-      rule = "Host(`{host}`) && HeadersRegexp(`cookie`, `.*remote-user={user_id}.*`)"
+    [http.routers.{id}]
+      rule = "Host(`{host}`) && HeadersRegexp(`cookie`,`.*remote-user={id}.*`)"
       middlewares = ["secured", "ssl-header"]
-      service = "code-server-{user_id}"
-      [http.routers.{user_id}.tls]
+      service = "code-server-{id}"
+      [http.routers.{id}.tls]
         certResolver = "le"
   [http.services]
-    [http.services.code-server-{user_id}]
-      [[http.services.code-server-{user_id}.loadBalancer.servers]]
-         url = "http://code-{user_id}:8080"
+    [http.services.code-server-{id}]
+      [[http.services.code-server-{id}.loadBalancer.servers]]
+         url = "http://code-{id}:8080"
 """
 
-    with open(f"/route_conf/{user_id}.toml", "wb") as toml_file:
+    with open(f"/route_conf/{id}.toml", "wb") as toml_file:
         toml_file.write(conf.encode('utf-8'))
-
-    states[user_id] = "READY"
 
     try:
         container = client.containers.get(code_name)
-        states[user_id] = 'READY'
+        states[id] = 'READY'
         print(container.id)
         return
     except docker.errors.NotFound:
         pass
 
     try:
-        volume = client.volumes.get(code_name)
+        client.volumes.get(code_name)
     except docker.errors.NotFound:
-        volume = client.volumes.create(code_name)
-    image = client.images.build(
+        client.volumes.create(code_name)
+
+    client.images.build(
         path='./coder',
         tag=f'code_server/{code_name}',
         pull=True,
         forcerm=True,
         nocache=True,
         buildargs={
-            'CODER_USER': user_id
+            'CODER_USER': id
         }
     )
 
@@ -89,7 +86,9 @@ async def start_code_server(user_id):
         name=code_name,
         detach=True,
         command=["--auth", "none"],
-        volumes={code_name: {'bind': f'/home/{user_id}/project', 'mode': 'rw'}},
+        volumes={
+            code_name: {'bind': f'/home/{id}/project', 'mode': 'rw'}
+        },
         network=docker_network,
     )
 
@@ -97,3 +96,5 @@ async def start_code_server(user_id):
     while container.id not in cids:
         cids = [c.id for c in client.containers.list()]
         await asyncio.sleep(3.0)
+
+    states[id] = "READY"
